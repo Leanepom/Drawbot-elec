4cbc0a06b7a5a86614b97dc6faf88395e367ea1d
Mathilde5 <149074812+Mathilde5@users.noreply.github.com>
2025-05-07 14:45:58 +0200

    Test robot

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,106 @@
-#include <WiFi.h>          // Gère la connexion Wi-Fi
-#include <WebServer.h>     // Crée un serveur web
+#include "BluetoothSerial.h"
 
-// Remplace par ton SSID et mot de passe
-const char* ssid = "OMNES Education";
-const char* password = "A{mA_dw4D";
+BluetoothSerial SerialBT;
 
-WiFiServer server(80);
+// Définition des broches (basé sur ton image)
+#define EN_D 23
+#define EN_G 4
+
+#define IN_1_D 19
+#define IN_2_D 18
+
+#define IN_1_G 17
+#define IN_2_G 16
+
+// Fonctions de mouvement
+void avancer() {
+  digitalWrite(IN_1_D, HIGH);
+  digitalWrite(IN_2_D, LOW);
+  digitalWrite(IN_1_G, HIGH);
+  digitalWrite(IN_2_G, LOW);
+}
+
+void reculer() {
+  digitalWrite(IN_1_D, LOW);
+  digitalWrite(IN_2_D, HIGH);
+  digitalWrite(IN_1_G, LOW);
+  digitalWrite(IN_2_G, HIGH);
+}
+
+void tournerGauche() {
+  digitalWrite(IN_1_D, HIGH);
+  digitalWrite(IN_2_D, LOW);
+  digitalWrite(IN_1_G, LOW);
+  digitalWrite(IN_2_G, HIGH);
+}
+
+void tournerDroite() {
+  digitalWrite(IN_1_D, LOW);
+  digitalWrite(IN_2_D, HIGH);
+  digitalWrite(IN_1_G, HIGH);
+  digitalWrite(IN_2_G, LOW);
+}
+
+void stopMotors() {
+  digitalWrite(IN_1_D, LOW);
+  digitalWrite(IN_2_D, LOW);
+  digitalWrite(IN_1_G, LOW);
+  digitalWrite(IN_2_G, LOW);
+}
 
 void setup() {
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
+  SerialBT.begin("ESP32_ROBOT_BT"); // Nom du module Bluetooth
 
-  // Attente de la connexion
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connexion au WiFi...");
-  }
+  // Configuration des broches moteurs
+  pinMode(EN_D, OUTPUT);
+  pinMode(EN_G, OUTPUT);
 
-  Serial.println("Connecté au WiFi !");
-  Serial.println(WiFi.localIP());
+  pinMode(IN_1_D, OUTPUT);
+  pinMode(IN_2_D, OUTPUT);
+  pinMode(IN_1_G, OUTPUT);
+  pinMode(IN_2_G, OUTPUT);
 
-  server.begin();
+  // Activation des moteurs au démarrage
+  digitalWrite(EN_D, HIGH);
+  digitalWrite(EN_G, HIGH);
+
+  stopMotors();
+
+  Serial.println("Bluetooth actif. Attente de commande...");
 }
 
 void loop() {
-  WiFiClient client = server.available();
-  if (client) {
-    Serial.println("Client connecté");
-    while (client.connected()) {
-      if (client.available()) {
-        String req = client.readStringUntil('\r');
-        Serial.println(req);
-        client.flush();
+  if (SerialBT.available()) {
+    char cmd = SerialBT.read();
+    Serial.print("Commande reçue : ");
+    Serial.println(cmd);
 
-        // Réponse simple HTTP
-        client.println("HTTP/1.1 200 OK");
-        client.println("Content-type:text/html");
-        client.println();
-        client.println("<h1>ESP32 Web Server</h1>");
-        client.println();
+    switch (cmd) {
+      case '1': // Tourner à droite
+        avancer();
+        SerialBT.println("Tourner à droite");
         break;
-      }
+      case '2': // Tourner à gauche
+        reculer();
+        SerialBT.println("Tourner à gauche");
+        break;
+      case '3': // Reculer
+        tournerGauche();
+        SerialBT.println("Reculer");
+        break;
+      case '4': // Avancer
+        tournerDroite();
+        SerialBT.println("Avancer");
+        break;
+      case '0': // Stop
+        stopMotors();
+        SerialBT.println("Stop");
+        break;
+      default:
+        SerialBT.println("Commande inconnue");
+        break;
     }
-    client.stop();
-    Serial.println("Client déconnecté");
   }
 }
+
